538a1574f7c8a63603e7e90f4eb18d63f1684d40
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-01-06 16:36:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,53 @@ const imageData = ()=>{
   return data;
 }
 
+let data = imageData()
+
+
 function App() {
   // code here
+
+  // Another way to style
+
+  // let galleryStyle = {
+  //   textAlign:"center"
+  // }
+
+  // return(
+  //   <>
+  //   <h1 style={galleryStyle}>Kalvium Gallery</h1>
+  //   </>
+  // )
+
+  let styleMydiv = {
+    display:"grid",
+    gridTemplateColumns:"repeat(2,1fr)",
+    margin:"30px"
+  }
+
+  return(
+    <>
+    <h1 style={{textAlign:"center"}}>Kalvium Gallery</h1>
+
+    <div style={styleMydiv}>
+      <img src={data[0].img} alt="" width={"600px"} />
+      <img src={data[1].img} alt="" width={"600px"} />
+      <img src={data[2].img} alt="" width={"600px"} />
+      <img src={data[3].img} alt="" width={"600px"} />
+    </div>
+
+    {/* Alternative way using map
+    <div style={styleMydiv}>
+      {data.map((el)=>(
+        <div key={el.id}>
+        <img src={el.img} width={"300px"} />
+        </div>
+      ))}
+
+    </div> */}
+
+    </>
+  )
 }
 
 export default App;
